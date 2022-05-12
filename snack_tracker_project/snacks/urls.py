ef6de7bf42249eafb120bc20c790291e8dab3902
Ayat <alkayedayat93@gmail.com>
2022-05-12 08:12:32 +0300

    edit view to empty path

--- snack_tracker_project/snacks/urls.py
+++ snack_tracker_project/snacks/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from .views import SnackDetailView , SnackListView
 
 urlpatterns = [
-    path('snack-list' , SnackListView.as_view() , name = 'snack-list'),
-    path('snack-detail/<int:pk>' , SnackDetailView.as_view() , name ='snack-detail')
+    path('' , SnackListView.as_view() , name = 'snack-list'),
+    path('<int:pk>/' , SnackDetailView.as_view() , name ='snack-detail')
 ]
